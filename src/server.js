b21451cafa5fbd20c2197b8d0d77618a7468c510
Denis Bochev <denisbo4ev@gmail.com>
2025-03-19 18:51:36 +0200

    add models user and session

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import notFoundHandler from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
-  const PORT = process.env.PORT || 4040;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
   app.use(cors());
