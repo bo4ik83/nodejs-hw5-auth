860cb2d4fe7b715572339ea89fc8d3f42844dbd2
Denis Bochev <denisbo4ev@gmail.com>
2025-03-20 20:44:19 +0200

    add validation

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,8 +4,18 @@ import { registerController } from '../controllers/auth.js';
 
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 
-const router = express.Router();
+import { registerSchema } from '../validation/user.js';
 
-router.use('/register', ctrlWrapper(registerController));
+import { validateBody } from '../middlewares/validateBody.js';
+
+const router = express.Router();
+const jsonParser = express.json();
+
+router.use(
+  '/register',
+  jsonParser,
+  validateBody(registerSchema),
+  ctrlWrapper(registerController),
+);
 
 export default router;
